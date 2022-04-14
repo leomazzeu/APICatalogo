bb583af2e5da2a4c5c574a67ffd84207fce080f0
Léo Mazzeu <leonardomazzeudasilva@gmail.com>
2022-04-14 17:38:45 -0300

    Initializing Product Collection

--- APICatalogo/Model/Categoria.cs
+++ APICatalogo/Model/Categoria.cs
@@ -1,7 +1,14 @@
-﻿namespace APICatalogo.Model
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Model
 {
     public class Categoria
     {
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+
         public int CategoriaId { get; set; }
         public string Nome { get; set; }
         public string ImagemUrl { get; set; }
