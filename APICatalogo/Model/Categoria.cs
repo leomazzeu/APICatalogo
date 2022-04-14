73b92257ba5802ada44f97278544aa9e0bb83f6d
Léo Mazzeu <leonardomazzeudasilva@gmail.com>
2022-04-14 17:04:25 -0300

    Add model Categoria

---
+++ APICatalogo/Model/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace APICatalogo.Model
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+        public string ImagemUrl { get; set; }
+    }
+}
