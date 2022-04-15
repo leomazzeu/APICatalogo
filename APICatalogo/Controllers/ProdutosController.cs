e20c04cbe5d0a1826aaca7485d017d31671466b2
Léo Mazzeu <leonardomazzeudasilva@gmail.com>
2022-04-15 10:25:24 -0300

    Created Produtos Controller

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,22 @@
+﻿using APICatalogo.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    public class ProdutosController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
