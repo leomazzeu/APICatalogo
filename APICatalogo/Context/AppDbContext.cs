0552c4cc973b66157a360e6d3f9dc7aa79636a17
Léo Mazzeu <leonardomazzeudasilva@gmail.com>
2022-04-15 00:33:40 -0300

    Define class AppDbContext public

--- APICatalogo/Context/AppDbContext.cs
+++ APICatalogo/Context/AppDbContext.cs
@@ -5,10 +5,11 @@ namespace APICatalogo.Context
 {
     public class AppDbContext : DbContext
     {
-        protected AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {}
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
 
-        public DbSet<Categoria>Categorias { get; set; }
-        public DbSet<Produto>Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
     }
 }
